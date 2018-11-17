5c7ecd862ae7768c9d53a2ca86ed4d310673626a
yl573 <yl573@cam.ac.uk>
2018-11-17 01:10:48 +0000

    added saving and loading

--- main/trainer.py
+++ main/trainer.py
@@ -2,16 +2,33 @@ from main.data.data_loaders import PlaneNetDataLoader
 from main.models.planenet import PlaneNet
 from main.args import parse_args
 import torch
-
+from tensorboardX import SummaryWriter
+import os
+import datetime
 from main.loss.losses import calc_plane_loss
+
 
 class Trainer:
 
     def __init__(self, args):
         self.data_loader = PlaneNetDataLoader(args.val_path, 'val', args.batchSize)
-        self.model = PlaneNet(args)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=args.LR)
         self.device = self.get_device()
+        self.model = PlaneNet(args)
+        if args.checkpoint:
+            print('Loading state dict from: {}'.format(args.checkpoint))
+            self.model.load_state_dict(torch.load(args.checkpoint))
+        self.model.to(self.device)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=args.LR)
+        self.save_dir = self.create_save_dir(args.log_dir, args.tag)
+        self.tensorboard = SummaryWriter(log_dir=self.save_dir, comment=args.tag)
+
+    def create_save_dir(self, log_dir, tag):
+        timestamp = str(datetime.datetime.utcnow()).replace(' ', '_')
+        dir_name = '{}_{}'.format(tag, timestamp)
+        dir_path = os.path.join(log_dir, dir_name)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        return dir_path
 
     def get_device(self):
         if torch.cuda.is_available():
@@ -28,23 +45,39 @@ class Trainer:
     def train(self):
         print('Training on device: {}'.format(self.device))
 
-        for i in range(10):
-            batch = next(self.data_loader)
-            batch = self.batch_to_device(batch)
-            planes, segmentation, depth = self.model(batch['image_norm'])
+        for epoch in range(args.numEpochs):
+            for iter in range(args.numTrainingImages):
+                current_iter = epoch * args.numTrainingImages + iter
 
-            plane_loss = calc_plane_loss(planes, batch['plane'])
+                batch = next(self.data_loader)
+                batch = self.batch_to_device(batch)
+                planes, segmentation, depth = self.model(batch['image_norm'])
 
-            loss = torch.sum(plane_loss)
+                plane_loss = calc_plane_loss(planes, batch['plane'])
 
-            print(loss.item())
+                loss = torch.sum(plane_loss)
 
-            loss.backward()
-            self.optimizer.step()
+                self.tensorboard.add_scalar('train/plane_loss', plane_loss.item(), current_iter)
+
+                if iter % args.printInterval == 0:
+                    print('epoch: {}, iter: {}, loss: {}'.format(epoch, iter, loss.item()))
+
+                loss.backward()
+                self.optimizer.step()
+
+            save_path = os.path.join(self.save_dir, 'checkpoint-latest')
+            torch.save(self.model.state_dict(), save_path)
 
 
 if __name__ == '__main__':
     args = parse_args()
     args.val_path = '/Volumes/MyPassport/planes_scannet_val.tfrecords'
+    args.log_dir = '/Users/yuxuanliu/Desktop/4YP/StructureSLAM/logs'
+    args.tag = 'test'
+    args.save_dir = '/Users/yuxuanliu/Desktop/4YP/StructureSLAM/logs/models'
+    args.checkpoint = '/Users/yuxuanliu/Desktop/4YP/StructureSLAM/logs/test_2018-11-17_01:04:44.301218/checkpoint-latest'
+    args.numTrainingImages = 10
+    args.numEpochs = 1
+    args.printInterval = 20
     trainer = Trainer(args)
     trainer.train()
