337864e129a5ffa98c648e6f4362a51b31bdc799
yl573 <yl573@cam.ac.uk>
2018-11-22 23:29:16 +0000

    added pretrained

--- main/models/planenet.py
+++ main/models/planenet.py
@@ -15,7 +15,7 @@ class PlaneNet(nn.Module):
         self.options = options
         drn_out_dim = options.drn_channels[-1]
 
-        self.drn = drn_d_54(pretrained=False, out_map=options.drn_out_map, num_classes=-1,
+        self.drn = drn_d_54(pretrained=True, out_map=options.drn_out_map, num_classes=-1,
                             out_middle=False, channels=options.drn_channels)
         # self.pool = torch.nn.AvgPool2d((int(32 * options.height / options.width), 32))
         self.plane_pred = nn.Linear(drn_out_dim, options.numOutputPlanes * 3)
